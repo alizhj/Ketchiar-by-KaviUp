f72022f6d8ade9e2db55b05d1ef5a789dd12ce8a
sundbergz <robinkaspersundberg@gmail.com>
2015-03-19 13:28:40 +0100

    Spel version 1!!
    Nu har jag fixat så att första delen av spelet är fixat, man trycker på
    den spelaren man vill ha och sedan fadeas bilderna ut och stora bilden
    på den personen man valt in!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,46 @@
 $(document).ready(function() {
 
-$(document).scroll(function() {
-      var y = $(this).scrollTop();
-      if (y >= 100) {
-        TweenMax.to($(".content"),2, {x: "0px", y: "800px"});
-      }  
-      else {
-      	TweenMax.to($(".content"),2, {x: "0px", y: "0px"});
-      }
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		if (y >= 100) {
+		TweenMax.to($(".content"),2, {x: "0px", y: "800px"});
+		}  
+		else {
+			TweenMax.to($(".content"),2, {x: "0px", y: "0px"});
+		}
     });
+
+    $(".headbox").click(function() {
+
+    	var playerPicture = ["url(Images/lisa.png)", "url(Images/tobbe.png)", "url(Images/robin.png)"];
+
+   		var clickedPlayer = $(this).attr("id");
+
+   		if (clickedPlayer == "lisa-head") {
+    		clickedPlayer = "Lisa";
+    	} else if (clickedPlayer == "tobbe-head") {
+    		clickedPlayer = "Tobbe";
+    	} else {
+    		clickedPlayer = "Robin";
+    	}
+
+   		$("#head-slogan").html("You picked" + " " + clickedPlayer);
+
+    	if (clickedPlayer == "Lisa") {
+    		clickedPlayer = 0;
+    	} else if (clickedPlayer == "Tobbe") {
+    		clickedPlayer = 1;
+    	} else {
+    		clickedPlayer = 2;
+    	}
+
+    	$("#head-slogan").delay(500).fadeOut(500);
+    	$(this).siblings().hide();
+    	$(this).delay(500).fadeOut(500, showPlayer);
+
+    	function showPlayer() {
+    		$("#player-wrapper").fadeIn().css("background-image", playerPicture[clickedPlayer]);
+    	}
+    });
+
 });
