dda1a0245ef7598bebff834282c24fd6343e161e
sundbergz <robinkaspersundberg@gmail.com>
2015-03-19 16:56:59 +0100

    Kommentarer JS
    La till kommentarer för JS om ni inte förstår!

--- js/main.js
+++ js/main.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
 			TweenMax.to($(".content"),2, {x: "0px", y: "0px"});
 		}
     });
-
+	//Dölj vid uppstart
     $("#character-wrapper").hide();
     $("#final-answer").hide();
 
-
+    //Om ja, visa alternativ och dölj Yes/No
     $("#yes").click(function() {	
     	optionYes()
     });
-
+    //Vid nej, ställ följdfråga
     $("#no").click(function() {
     	optionNo()
     });
@@ -27,7 +27,10 @@ $(document).ready(function() {
     	$("#character-wrapper").show();
     	$("#head-slogan").html("Pick your player");
 	}
-
+	/*Om nej väljs, fråga "Are you sure?"" Om nej väljs på den frågan så rullar funktionen
+	för ja (optionYes) men om de svarar ja så skriver den ut "But it's fun" och de gamla värdena
+	skrivs över och några nya läggs till. DENNA FUNKTIONEN KAN NOG BLI BÄTTRE HAHA. Men nu funkar det iaf
+	*/
 	function optionNo() {
 		$("#head-slogan").html("Are you sure?");
 		$("#no").click(function() {	
@@ -35,7 +38,7 @@ $(document).ready(function() {
     	});
 
     	$("#yes").click(function() {	
-    		$("#head-slogan").html("But its fun!");
+    		$("#head-slogan").html("But it's fun!");
     		$("#character-wrapper").hide();
     		$("#button-wrapper").hide();
     		$("#final-answer").show();
@@ -45,13 +48,13 @@ $(document).ready(function() {
     		});
     	});
 	}
-
+	//Klickar på något huvud
     $(".headbox").click(function() {
-
+    	//Två variabler skapas, ena för länk till bilderna, andra för vilken av "headbox" som valdes
     	var playerPicture = ["url(Images/lisa.png)", "url(Images/tobbe.png)", "url(Images/robin.png)"];
 
    		var clickedPlayer = $(this).attr("id");
-
+   		//Gör om svaret för att kunna skriva ut namnet
    		if (clickedPlayer == "lisa-head") {
     		clickedPlayer = "Lisa";
     	} else if (clickedPlayer == "tobbe-head") {
@@ -59,9 +62,11 @@ $(document).ready(function() {
     	} else {
     		clickedPlayer = "Robin";
     	}
-
+    	//Skriv ut "You picked " samt namnet
    		$("#head-slogan").html("You picked" + " " + clickedPlayer);
-
+   		/*Gör om namnen till booleans för att kunna välja plats i array
+   		(notera att numren är beroende på vilken plats de har i arrayen)
+   		*/
     	if (clickedPlayer == "Lisa") {
     		clickedPlayer = 0;
     	} else if (clickedPlayer == "Tobbe") {
@@ -69,11 +74,11 @@ $(document).ready(function() {
     	} else {
     		clickedPlayer = 2;
     	}
-
+    	//När man trycker så visas huvudet man valt samt namnet .5 sek innan showPlayer funktionen hämtas
     	$("#head-slogan").delay(500).fadeOut(500);
     	$(this).siblings().hide();
     	$(this).delay(500).fadeOut(500, showPlayer);
-
+    	//Fade in på bilden och väljer en background image (med rätt värde från arrayen)
     	function showPlayer() {
     		$("#player-wrapper").fadeIn().css("background-image", playerPicture[clickedPlayer]);
     	}
